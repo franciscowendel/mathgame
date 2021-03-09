47c251a4c7053bfdba53891334b1bcb64f3ee76e
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-09 02:51:07 -0300

    Implementando o arquivo 'game.py'

--- game/game.py
+++ game/game.py
@@ -9,6 +9,11 @@ def main():
 def jogar(pontos):
     try:
         dificuldade: int = int(input('DIFICULDADE: [1 - FÁCIL, 2 - MÉDIA, 3 - DIFÍCIL, 4 - MUITO DIFÍCIL] '))
+        ope: int = int(input('OPERAÇÃO: [1 - SOMA, 2 - SUBTRAÇÃO, 3 - MULTIPLICAÇÃO]'))
+        if ope > 3:
+            print('Operação inválida...')
+            main()
+
     except (ValueError, TypeError, UnboundLocalError) as err:
         return f'Erro do tipo {err} encontrado'
 
